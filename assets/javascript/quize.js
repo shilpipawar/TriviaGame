86f33a0144e7b014509f074ad4fd1bddbf8418ae
shilpipawar <shilpipawar@gmail.com>
2018-10-19 00:33:05 -0500

    New Changes

--- assets/javascript/quize.js
+++ assets/javascript/quize.js
@@ -1,3 +1,115 @@
 $(document).ready(function () {
+    //Declare ojects for all Question and Ans
+    //////////Que Object Declaration/////////////////////////////////////////////////
+    var Que1 = {
+        question: "Highest mountain of world 'Mount Everest' is located in",
+        answers: {
+            A: "Asia",
+            B: "Antarctic",
+            C: "Australia",
+            D: "North America"
+        },
+        correctAnswer: "A"
+    };
 
+    var Que2 = {
+        question: "North America and Caribbean Sea lies to northwest from",
+        answers: {
+            A: "Europe",
+            B: "Asia",
+            C: "Africa",
+            D: "South America"
+        },
+        correctAnswer: "D"
+    };
+
+    var Que3 = {
+        question: "North America shares boundary with Pacific Ocean from",
+        answers: {
+            A: "East"
+        B: "West",
+            C: "North",
+            D: "South"
+        },
+        correctAnswer: "B"
+    };
+
+    var Que4 = {
+        question: "World's second largest continent is",
+        answers: {
+            A: "Asia",
+            B: "Africa",
+            C: "Antarctica",
+            D: "Australia"
+        },
+        correctAnswer: "B"
+    };
+
+    var Que5 = {
+        question: "World's second largest continent is",
+        answers: { a: "x", b: "y", c: "z", d: "u" },
+        correctAnswer: "c"
+    };
+
+    var Que6 = {
+        question: "Continent 'Asia' shares its East border with",
+        answers: {
+            A: "Africa",
+            B: "Arctic Ocean",
+            C: "Pacific Ocean",
+            D: "Ural Mountains"
+        },
+        correctAnswer: "D"
+    };
+
+    var Que7 = {
+        question: "Longest river in 'Asia' is",
+        answers: {
+            A: "Mahaveli of Srilanka",
+            B: "Ganga of India",
+            C: "Yangtze of China",
+            D: "Huang-He of China"
+        },
+        correctAnswer: "C"
+    };
+
+    var Que8 = {
+        question: "Largest suspension bridge 'Golden Gate Bridge' is located in North America continent at",
+        answers: {
+            A: "San Francisco",
+            B: "Panama",
+            C: "Cuba",
+            D: "Venice"
+        },
+        correctAnswer: "A"
+    };
+
+    var Que9 = {
+        question: "Phenomenon in which glow is observed in sky at night called Aurora Australia is also classified as",
+        answers: {
+            A: "Western Lights",
+            B: "Eastern Lights",
+            C: "Southern Lights",
+            D: "Northern Lights"
+        },
+        correctAnswer: "C"
+    };
+
+    var Que10 = {
+        question: "Continent which has no active volcanic regions is",
+        answers: {
+            A: "North America",
+            B: "Europe",
+            C: "Australia",
+            D: "Africa"
+        },
+        correctAnswer: "C"
+    };
+
+    function CreateQuizPage(){
+        
+    }
+    /////////////////////////////////////Call Function on Page Load/////////////////////////////////////////////////////////////////////////////////
+    //Function to load question HTML
+    CreateQuizPage();
 });
