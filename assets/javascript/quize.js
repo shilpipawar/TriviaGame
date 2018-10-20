92655a08009c4b4d4f9d083708c39ce4a780d4ec
shilpipawar <shilpipawar@gmail.com>
2018-10-20 01:40:25 -0500

    New Changes

--- assets/javascript/quize.js
+++ assets/javascript/quize.js
@@ -26,8 +26,8 @@ $(document).ready(function () {
     var Que3 = {
         question: "North America shares boundary with Pacific Ocean from",
         answers: {
-            A: "East"
-        B: "West",
+            A: "East",
+            B: "West",
             C: "North",
             D: "South"
         },
@@ -46,9 +46,13 @@ $(document).ready(function () {
     };
 
     var Que5 = {
-        question: "World's second largest continent is",
-        answers: { a: "x", b: "y", c: "z", d: "u" },
-        correctAnswer: "c"
+        question: "Sea which separates Africa from Asia is",
+        answers: { 
+            A: "Baltic Sea", 
+            B: "Black Sea",
+            C: "Mediterranean Sea", 
+            D: "Red Sea" },
+        correctAnswer: "D"
     };
 
     var Que6 = {
@@ -106,10 +110,80 @@ $(document).ready(function () {
         correctAnswer: "C"
     };
 
-    function CreateQuizPage(){
-        
+    //Variable declaration for HTML element ref for showing question, awnser and correct awnser
+    var questionArray = [Que1, Que2, Que3, Que4, Que5, Que6, Que7, Que8, Que9, Que10];
+    var questionPlaceholder = $('.Question');
+    var awnserPlaceHolder = $('.Option1');
+    var correctAnswerPlaceHolder = $('.CorrectAnw');
+
+
+    // ///Function for creating
+    function CreateQuizPage(obj) {
+
+        //var outputHTML = [];
+        //Que
+        var temp = obj.question;
+        console.log(temp);
+        var output = $('<div class ="title">' + temp + '</div>');
+        console.log(obj.answers.A);
+        var opt = [obj.answers.A, obj.answers.B, obj.answers.C, obj.answers.D];
+
+        console.log(temp);
+        questionPlaceholder.append(output);
+        //Ans
+        for (i = 0; i < opt.length; i++) {
+            var radioBtn = $('<input type="radio" name="radio-choice-1" class ="radioType">' + opt[i] + '</input>' + '<br>');
+            radioBtn.appendTo('#target');
+        };
+
     }
+
+    function LoadQuizQue(array) {
+        console.log("LoadQuizQue Enter:")
+        for (var j = 0; j < array.length; j++) {
+            console.log(array[j]);
+        };
+    }
+
+    //Timer
+    var id = 0;
+    window.setInterval(function(){
+        /// call your function here
+        ClearALl();
+        $('.Timer').append("Timer: " + 45);
+       var txt = "Question " + id + 1;
+       $('.Question').append(txt);
+        if(id < questionArray.length){
+            CreateQuizPage(questionArray[id]);
+            id++;
+        }else{
+            //Restart the quiz
+        
+            var BtnRestart = $('<a class="btn btn-primary btn-lg restartbtn" href="index.html" role="button">Restart</a>');
+            BtnRestart.appendTo('.CorrectAnw');
+        }
+        
+ }, 500);
+
+ function ClearALl(){
+    $('#target').empty();
+    $('.title').empty();
+    $('.restartbtn').empty();
+    $('.Timer').empty();
+    $('.Question').empty();
+    $('.CorrectAnw').empty();
+ }
+    //$("input[type='radio']").attr("checked",true).checkboxradio("refresh");
+
     /////////////////////////////////////Call Function on Page Load/////////////////////////////////////////////////////////////////////////////////
-    //Function to load question HTML
-    CreateQuizPage();
+
+    //function starttimer45() inside that call function quizseq 
+    //inside quizseq call function CreateQuizPage
+    //once user selects option call matchAnwser function
+    //if user is correct show msg congratulation
+    //if answer wrong show sad smaily with correct ans
+    //load new question.
+    //CreateQuizPage(Que10);//Test
+    //LoadQuizQue(questionArray);
+
 });
